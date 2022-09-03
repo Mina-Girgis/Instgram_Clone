e2ffbb230cd42615c0261a8af1d74de15ec13c92
Mina <minagirgisalfy@gmail.com>
2022-09-03 18:48:30 +0200

    login error design fixed

--- lib/modules/login_screen/login_screen.dart
+++ lib/modules/login_screen/login_screen.dart
@@ -21,211 +21,84 @@ class LoginScreen extends StatelessWidget {
             constraints: BoxConstraints(
               minHeight: SizeConfig.screenHeight! -90,
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: SizeConfig.screenHeight!*0.14,
-                ),
-                Container(
-                  height: SizeConfig.screenHeight!*0.7,
-                  alignment: Alignment.center,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                        left: SizeConfig.defaultSize! * 2,
-                        right: SizeConfig.defaultSize! * 2),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/instaWhite.png',
-                          width: SizeConfig.screenWidth! / 1.8,
-                        ),
-                        SizedBox(
-                          height: SizeConfig.defaultSize! * 3,
-                        ),
-                        defaultTextField(
-                            hintText: "Phone number, email or username",
-                            controller: emailController),
-                        SizedBox(
-                          height: SizeConfig.defaultSize! * 1.5,
-                        ),
-                        defaultTextField(
-                            hintText: "Password",
-                            controller: passwordController,
-                            obscure: true),
-                        SizedBox(
-                          height: SizeConfig.defaultSize! * 1.5,
-                        ),
-                        ButtonTheme(
-                            minWidth: SizeConfig.screenWidth!,
-                            height: SizeConfig.defaultSize! * 5,
-                            child: RaisedButton(
-                              onPressed: () {},
-                              color: Color.fromRGBO(0, 148, 245, 1.0),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Text(
-                                "Log in",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            )),
-                        SizedBox(
-                          height: SizeConfig.defaultSize! * 1,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Forgot your login details?",
-                              style: TextStyle(
-                                color: Color.fromRGBO(
-                                    129, 128, 128, 0.8235294117647058),
-                              ),
-                            ),
-                            TextButton(
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Spacer(),
+                  Container(
+                    height: SizeConfig.screenHeight!*0.14,
+                  ),
+                  Container(
+                    // height: SizeConfig.screenHeight!*0.7,
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          left: SizeConfig.defaultSize! * 2,
+                          right: SizeConfig.defaultSize! * 2),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/instaWhite.png',
+                            width: SizeConfig.screenWidth! / 1.8,
+                          ),
+                          SizedBox(
+                            height: SizeConfig.defaultSize! * 3,
+                          ),
+                          defaultTextField(
+                              hintText: "Phone number, email or username",
+                              controller: emailController),
+                          SizedBox(
+                            height: SizeConfig.defaultSize! * 1.5,
+                          ),
+                          defaultTextField(
+                              hintText: "Password",
+                              controller: passwordController,
+                              obscure: true),
+                          SizedBox(
+                            height: SizeConfig.defaultSize! * 1.5,
+                          ),
+                          ButtonTheme(
+                              minWidth: SizeConfig.screenWidth!,
+                              height: SizeConfig.defaultSize! * 5,
+                              child: RaisedButton(
                                 onPressed: () {},
+                                color: Color.fromRGBO(0, 148, 245, 1.0),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
                                 child: Text(
-                                  "Get help loggin in.",
+                                  "Log in",
                                   style: TextStyle(
                                     color: Colors.white,
-                                    fontWeight: FontWeight.bold,
                                   ),
-                                )),
-                          ],
-                        ),
-                        Row(children: [
-                          Expanded(
-                              child: Divider(
-                            color: Colors.grey,
-                            thickness: 0.9,
-                          )),
-                          Text(
-                            "  OR  ",
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Expanded(
-                              child: Divider(
-                            color: Colors.grey,
-                            thickness: 0.9,
-                          )),
-                        ]),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              FontAwesomeIcons.facebook,
-                              color: Colors.blue,
-                              size: 35.0,
-                            ),
-                            TextButton(
-                              onPressed: () {},
-                              child: Text("Log in with Facebook"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: SizeConfig.screenHeight!*0.1,
-                  alignment: Alignment.bottomCenter,
-                  // color: Colors.red,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Expanded(
-                              child: Divider(
-                                color: Colors.grey,
-                                thickness: 0.0,
-                              )),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account?"),
-                          TextButton(
-                              onPressed: (){},
-                              child: Text(
-                                "Sign up.",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
                                 ),
                               )),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-/*Padding(
-                  padding: EdgeInsets.only(left:SizeConfig.defaultSize!*2, right: SizeConfig.defaultSize!*2),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/instaWhite.png',
-                        width: SizeConfig.screenWidth!/1.8,
-                      ),
-                      SizedBox(height: SizeConfig.defaultSize!*3,),
-                      defaultTextField(hintText: "Phone number, email or username" , controller: emailController),
-                      SizedBox(height: SizeConfig.defaultSize!*1.5,),
-                      defaultTextField(hintText: "Password" , controller: passwordController ,obscure: true),
-                      SizedBox(height: SizeConfig.defaultSize!*1.5,),
-                      ButtonTheme(
-                          minWidth: SizeConfig.screenWidth!,
-                          height: SizeConfig.defaultSize!*5,
-                          child: RaisedButton(
-                            onPressed: (){},
-                            color: Color.fromRGBO(0, 148, 245, 1.0),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text(
-                              "Log in",
-                              style: TextStyle(
-                                color: Colors.white,
+                          SizedBox(
+                            height: SizeConfig.defaultSize! * 1,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Forgot your login details?",
+                                style: TextStyle(
+                                  color: Color.fromRGBO(
+                                      129, 128, 128, 0.8235294117647058),
+                                ),
                               ),
-                            ),
-                          )),
-                      SizedBox(height: SizeConfig.defaultSize!*1,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Forgot your login details?",
-                            style: TextStyle(
-                              color: Color.fromRGBO(129, 128, 128, 0.8235294117647058),
-                            ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    "Get help loggin in.",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )),
+                            ],
                           ),
-                          TextButton(
-                              onPressed: (){},
-                              child: Text(
-                                "Get help loggin in.",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )),
-                        ],
-                      ),
-                      Row(
-                          children: [
+                          Row(children: [
                             Expanded(
                                 child: Divider(
                                   color: Colors.grey,
@@ -243,55 +116,65 @@ class LoginScreen extends StatelessWidget {
                                   thickness: 0.9,
                                 )),
                           ]),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(FontAwesomeIcons.facebook,
-                            color: Colors.blue,
-                            size: 35.0,
-                          ),
-                          TextButton(
-                            onPressed: (){},
-                            child: Text("Log in with Facebook"),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                FontAwesomeIcons.facebook,
+                                color: Colors.blue,
+                                size: 35.0,
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text("Log in with Facebook"),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-
-
-                */
-
-/*Column(
-                  children: [
-                    Row(
+                  Spacer(),
+                  Container(
+                    // height: SizeConfig.screenHeight!*0.1,
+                    alignment: Alignment.bottomCenter,
+                    // color: Colors.red,
+                    child: Column(
                       children: [
-                        Expanded(
-                            child: Divider(
-                              color: Colors.grey,
-                              thickness: 0.0,
-                            )),
+                        Row(
+                          children: [
+                            Expanded(
+                                child: Divider(
+                                  color: Colors.grey,
+                                  thickness: 0.0,
+                                )),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account?"),
+                            TextButton(
+                                onPressed: (){},
+                                child: Text(
+                                  "Sign up.",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                )),
+                          ],
+                        ),
                       ],
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Don't have an account?"),
-                        TextButton(
-                            onPressed: (){},
-                            child: Text(
-                              "Sign up.",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
-
-
-                */
