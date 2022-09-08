98bcf906527022b219df3507954faa1f61ce3e47
Mina <minagirgisalfy@gmail.com>
2022-09-08 18:05:18 +0200

    ProfileScreen grid view done

--- lib/modules/home/screens/profile_screen.dart
+++ lib/modules/home/screens/profile_screen.dart
@@ -6,140 +6,216 @@ import 'package:pro/shared/components/constants.dart';
 
 import '../../../services/utils/size_config.dart';
 import '../components/components.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          title: Row(
-            children: [
-              Icon(FontAwesomeIcons.lock,size: 20.0,),
-              SizedBox(width: 8.0,),
-              Text("mina_girgis",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            title: Row(
+              children: [
+                Icon(
+                  FontAwesomeIcons.lock,
+                  size: 20.0,
                 ),
-              ),
-              ButtonTheme(
-                child: IconButton(
-                  splashRadius: 15,
-                  iconSize: 30.0,
-                  alignment: Alignment.centerLeft,
-                  onPressed: () {},
-                  icon: Icon(
-                    IconData(
-                      0xf13d,
-                      fontFamily: 'MaterialIcons',
+                SizedBox(
+                  width: 8.0,
+                ),
+                Text(
+                  "mina_girgis",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ButtonTheme(
+                  child: IconButton(
+                    splashRadius: 15,
+                    iconSize: 30.0,
+                    alignment: Alignment.centerLeft,
+                    onPressed: () {},
+                    icon: Icon(
+                      IconData(
+                        0xf13d,
+                        fontFamily: 'MaterialIcons',
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            actions: [
+              IconButton(
+                iconSize: 30.0,
+                splashRadius: 15,
+                onPressed: () {},
+                icon: Icon(
+                  Icons.add_box_outlined,
+                ),
               ),
+              IconButton(
+                iconSize: 30.0,
+                splashRadius: 15,
+                onPressed: () {},
+                icon: Icon(
+                  Icons.menu,
+                  size: 35.0,
+                ),
+              ),
+              SizedBox(width: 5,),
             ],
           ),
-          actions: [
-            IconButton(
-              iconSize: 30.0,
-              splashRadius: 15,
-              onPressed: () {},
-              icon: Icon(
-                Icons.add_box_outlined,
-              ),
-            ),
-            IconButton(
-              iconSize: 30.0,
-              splashRadius: 15,
-              onPressed: () {},
-              icon: Icon(
-                Icons.menu,
-                size: 35.0,
-              ),
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 10.0 ,right: 10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  profilePicWithOvelCircle(
-                    radius: 50.0,
-                    size: SizeConfig.defaultSize!*11,
-                    ovelCircle: true,
-                    padding: 0,
-                  ),
-                  profileNumbers(text:'Posts' ,number: 5),
-                  profileNumbers(text:'Followers' ,number: 307),
-                  profileNumbers(text:'Following' ,number: 639),
+          body: LayoutBuilder(
+            builder: (context, constraint) {
+              return SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0,right: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                profilePicWithOvelCircle(
+                                  radius: 50.0,
+                                  size: SizeConfig.defaultSize! * 11,
+                                  ovelCircle: true,
+                                  padding: 0,
+                                ),
+                                profileNumbers(text: 'Posts', number: 5),
+                                profileNumbers(text: 'Followers', number: 307),
+                                profileNumbers(text: 'Following', number: 639),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("Bio ..."),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: defaultButton(
+                                    child: Text(
+                                      "Edit profile",
+                                      style: TextStyle(
+                                        color: WHITE,
+                                      ),
+                                    ),
+                                    color: Color.fromRGBO(30, 30, 30, 1.0),
+                                    function: () {},
+                                    height: 4,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Container(
+                                  width: 40.0,
+                                  height: SizeConfig.defaultSize! * 4,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromRGBO(30, 30, 30, 1.0),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: IconButton(
+                                      splashRadius: 25.0,
+                                      onPressed: () {},
+                                      icon: Icon(
+                                        FontAwesomeIcons.user,
+                                        color: WHITE,
+                                        size: 20.0,
+                                      )),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              // color: Colors.grey,
+                              height: 130.0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                                child: ListView.separated(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 10,
+                                  separatorBuilder: (context, index) {
+                                    return SizedBox(
+                                      width: 15.0,
+                                    );
+                                  },
+                                  itemBuilder: (context, index) {
+                                    return storyDesignItem(ovel: false);
+                                  },
+                                ),
+                              ),
+                            ),
 
-                ],
-              ),
-              SizedBox(height: 10,),
-              Text("Bio ..."),
-              SizedBox(height: 10,),
-              Row(
-                children: [
-                  Expanded(
-                    child: defaultButton(
-                        child: Text("Edit profile",
-                          style: TextStyle(
-                            color: WHITE,
+                          ],
+                        ),
+                      ),
+
+
+                      DefaultTabController(
+                        length: 2,
+                        child: SizedBox(
+                          height: 50,
+                          child: AppBar(
+                            backgroundColor: Colors.transparent,
+
+                            bottom: TabBar(
+                              indicatorColor: WHITE,
+                              onTap: (index) {
+                                print(index);
+                              },
+                              physics: ScrollPhysics(),
+                              isScrollable: false,
+                              tabs: [
+                                Tab(
+                                  icon: Icon(Icons.directions_bike),
+                                ),
+                                Tab(
+                                  icon: Icon(
+                                    Icons.directions_car,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                        color: Color.fromRGBO(30, 30, 30, 1.0),
-                        function: (){},
-                        height: 4,
-                    ),
-                  ),
-                  SizedBox(width: 10,),
-                  Container(
-                    width: 40.0,
-                    height: SizeConfig.defaultSize!*4,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(30, 30, 30, 1.0),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: IconButton(
-                      splashRadius: 25.0,
-                      onPressed: (){},
-                      icon :Icon(FontAwesomeIcons.user,color: WHITE,size: 20.0,)
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20,),
-              Container(
-                // color: Colors.grey,
-                height: 130.0,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 10,
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        width: 15.0,
-                      );
-                    },
-                    itemBuilder: (context, index) {
-                      return storyDesignItem(ovel: false);
-                    },
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-
-            ],
+                      ),
+                      SizedBox(height: 5,),
+                      GridView.count(
+                        shrinkWrap: true,
+                        physics: ScrollPhysics(),
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 4.0,
+                        crossAxisSpacing: 5.0,
+                        children: List.generate(10, (index){
+                          return Container(
+                            width: SizeConfig.screenWidth!/3,
+                            height: 100,
+                            color: Colors.grey,
+                            child: Image.asset('assets/postImage.jpg',
+                              fit: BoxFit.cover,
+                            ),
+                          );
+                        }),
+                      ),
+                    ]),
+              );
+            },
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
+
